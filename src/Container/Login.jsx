2f8b0882a9d58116ae325729cefa1d392d015a89
SahdyTiger008 <chatterjeesoumyajeet@gmail.com>
2023-08-26 18:18:16 +0530

    Login page background change

--- src/Container/Login.jsx
+++ src/Container/Login.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import MusicBg from "../img/musicbg.jpg";
+// import MusicBg from "../img/musicbg.jpg";
 import { FcGoogle } from "react-icons/fc";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { firebaseApp } from "../firebase-config";
 import { useNavigate } from "react-router-dom";
+import LoginBg from '../img/loginBg.mp4';
 
 const Login = () => {
   const firebaseAuth = getAuth(firebaseApp);
@@ -24,9 +25,18 @@ const Login = () => {
   };
   return (
     <div className="flex justify-center items-center w-[100vw] h-[100vh] relative">
-      <img src={MusicBg} alt="" className="object-cover w-full h-full" />
+      {/* <img src={MusicBg} alt="" className="object-cover w-full h-full" /> */}
+      <video
+        loop
+        muted
+        autoPlay
+        className="absolute inset-0 object-cover w-full h-full">
+        <source src={LoginBg} type="video/mp4" />
+      </video>
       <div className="flex absolute w-[100vw] h-[100vh] bg-white/20 top-0 left-0 justify-center items-center">
-        <button className="flex justify-center items-center shadow-lg space-x-2 px-5 py-2 rounded-lg bg-white/40 hover:bg-white/60" onClick={() => login()}>
+        <button
+          className="flex justify-center items-center shadow-lg space-x-2 px-5 py-2 rounded-lg bg-white/40 hover:bg-white/60"
+          onClick={() => login()}>
           <FcGoogle fontSize={"25px"} />
           <p className="text-white font-semibold">Sign in with Google</p>
         </button>
