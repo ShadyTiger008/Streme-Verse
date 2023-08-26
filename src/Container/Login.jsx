238ddc3eddd18e01a1b3f98c3080b62a7e54310b
SahdyTiger008 <chatterjeesoumyajeet@gmail.com>
2023-08-26 18:44:36 +0530

    Trying to fix the login popup for small screen

--- src/Container/Login.jsx
+++ src/Container/Login.jsx
@@ -5,7 +5,7 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { firebaseApp } from "../firebase-config";
 import { useNavigate } from "react-router-dom";
-import LoginBg from '../img/loginBg.mp4';
+import LoginBg from "../img/loginBg.mp4";
 
 const Login = () => {
   const firebaseAuth = getAuth(firebaseApp);
@@ -16,15 +16,18 @@ const Login = () => {
   const login = async () => {
     const { user } = await signInWithPopup(firebaseAuth, provider);
     const { refreshToken, providerData } = user;
-    console.log(refreshToken, providerData); 
-    localStorage.setItem('user', JSON.stringify(providerData));
-    localStorage.setItem('accessToken', JSON.stringify(refreshToken));
+    console.log(refreshToken, providerData);
+    localStorage.setItem("user", JSON.stringify(providerData));
+    localStorage.setItem("accessToken", JSON.stringify(refreshToken));
 
-    await setDoc(doc(firebaseDb, 'users', providerData[0].uid),providerData[0]);
-    navigate('/', {replace: true});
+    await setDoc(
+      doc(firebaseDb, "users", providerData[0].uid),
+      providerData[0]
+    );
+    navigate("/", { replace: true });
   };
   return (
-    <div className="flex justify-center items-center w-[100vw] h-[100vh] relative">
+    <div className="flex justify-center items-center w-screen h-screen relative">
       {/* <img src={MusicBg} alt="" className="object-cover w-full h-full" /> */}
       <video
         loop
@@ -33,7 +36,7 @@ const Login = () => {
         className="absolute inset-0 object-cover w-full h-full">
         <source src={LoginBg} type="video/mp4" />
       </video>
-      <div className="flex absolute w-[100vw] h-[100vh] bg-white/20 top-0 left-0 justify-center items-center">
+      <div className="flex absolute w-screen h-screen bg-white/20 top-0 left-0 justify-center items-center">
         <button
           className="flex justify-center items-center shadow-lg space-x-2 px-5 py-2 rounded-lg bg-white/40 hover:bg-white/60"
           onClick={() => login()}>
